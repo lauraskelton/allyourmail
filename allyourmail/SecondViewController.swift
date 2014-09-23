d074f70eb431641d4512ff36ac94e68f306350e1
Laura Skelton <lauraeskelton@gmail.com>
2014-09-23 10:23:42 -0700

    Initial Commit

---
+++ allyourmail/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  allyourmail
+//
+//  Created by Laura Skelton on 9/23/14.
+//  Copyright (c) 2014 skelovenko. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
